eeb142da35b18a786d5c45aa6c97e853a2e521af
Felipe Gerolomo <felipegerolomo@gmail.com>
2020-06-29 23:16:31 -0300

    #AJUSTE PERFORMANCE

--- js/main.js
+++ js/main.js
@@ -61,9 +61,3 @@ var startVimeo = function () {
     '<iframe src="https://www.youtube-nocookie.com/embed/vPtbYmR2Xhk?feature=oembed&start&end&wmode=opaque&loop=0&controls=0&mute=0&rel=0&modestbranding=0&autoplay=1" allow="autoplay; fullscreen" allowfullscreen></iframe>';
 };
 
-var startYouTube = function () {
-  $("#thumb-youtube").css("display", "none");
-  $("#youtube-video").css("display", "block");
-  document.getElementById("youtube-video").innerHTML =
-    '<iframe class="elementor-video-iframe" allow="autoplay; fullscreen" allowfullscreen src="https://www.youtube.com/embed/0h4WRXc_OHw?autoplay=1&feature=oembed&amp;start&amp;end&amp;wmode=opaque&amp;loop=0&amp;controls=0&amp;mute=0&amp;rel=0&amp;modestbranding=0"></iframe>';
-};
